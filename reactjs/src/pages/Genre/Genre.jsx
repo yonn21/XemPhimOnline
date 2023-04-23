fbf8b16a5b3def709f1e88ab944387319607f486
PDM2112 <phodinhmanh2002@gmail.com>
2023-04-23 21:48:04 +0700

    update

--- reactjs/src/pages/Genre/Genre.jsx
+++ reactjs/src/pages/Genre/Genre.jsx
@@ -23,14 +23,12 @@ export default function Genre({ match }) {
             <h4 className='mr-5'>
                 PHIM HÀNH ĐỘNG
             </h4>
-
             <div className="">
                 <i className="fa fa-home ml-3 mr-2 pt-2 pb-2 color" />
                 <p className='m-0 mr-2 pt-2 pb-2 color'>Phim Mới</p>
                 <i className="fa fa-angle-right mr-2 pt-2 pb-2 color" />
                 <p className='m-0 mr-2 pt-2 pb-2'>{category}</p>
             </div>
-
             {renderFilm()}
         </div>
     )
